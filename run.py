cbfb79290397cbe4499bb6e8fe9a45761a28669f
Frida Wikell <frida.wikell@gmail.com>
2024-01-11 11:08:15 +0100

    Add: Move start page into a function

--- run.py
+++ run.py
@@ -2,17 +2,28 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# Start view
 def print_ascii(filename):
+    '''Opens ascii images and print them'''
+
     file = open(filename, "r")
     print(''.join([line for line in file]))
 
 
+def start_page():
+    '''Views the start page with ascii images'''
+
+    print_ascii("snake-ascii.txt")
+    print_ascii("welcome-ascii.txt")
+    input("Press Enter to continue...")
+
+
 def enter_name():
     player_name = input("Please enter your name: ")
-  
 
-print_ascii("snake-ascii.txt")
-print_ascii("welcome-ascii.txt")
-input("Press Enter to continue...")
+
+def main():
+    start_page()
+
+
+main()
 enter_name()
