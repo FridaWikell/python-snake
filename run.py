55dce6697167277d70ff0ef8eefa4dbc463debc7
Frida Wikell <frida.wikell@gmail.com>
2024-01-11 11:37:10 +0100

    Add: Rules page with entering name

--- run.py
+++ run.py
@@ -21,9 +21,24 @@ def enter_name():
     player_name = input("Please enter your name: ")
 
 
+def rules_page():
+    print("Rules:\n")
+    print("The rules are simple. You are the snake. So far, so good. Right? "
+          "You control the snake with your arrow keys. Up make the snake to"
+          "turn up, right makes the snake to turn right. Well, you get the picture."
+          "The goal is to eat as many of the lovely apples (o) as you can. "
+          "When you eat an apple, you grow and get one apple longer than before.\n"
+          "Beware of yourself! Don't collide into yourself!\n"
+          "Beware of the walls! Don't collide into the walls!\n"
+          "Easy peasy lemon squeezy! Right?\n")
+    enter_name()
+
+
 def main():
     start_page()
+    rules_page()
 
 
 main()
-enter_name()
+
+
