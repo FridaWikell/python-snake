a628b73bef06a8a53288b9a36db88ee8144fdea3
Frida Wikell <frida.wikell@gmail.com>
2024-01-12 09:28:08 +0100

    Fix: Edit gspread client name

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+SHEET = GSPREAD_CLIENT.open('snake_highscore')
 
 
 def print_ascii(filename):
